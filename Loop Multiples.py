c38b0fe5babc122a15f92662264d3e70c7fe8803
danelduranxs <107886582+danelduranxs@users.noreply.github.com>
2022-06-22 15:18:18 -0500

    Add files via upload

---
+++ Loop Multiples.py
@@ -0,0 +1,23 @@
+print()
+print("¡Tabla de números enteros!")
+print()
+
+s = float(input("Por favor escribir un número:"))
+z = int(input("Cuántos múltiplos (enteros) mostrar en pantalla:"))
+
+print()
+
+
+for r in range(1,z+1):
+    print(s * r)
+    
+    
+    
+
+
+
+
+
+
+
+
